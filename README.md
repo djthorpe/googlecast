1c068ce68e8b01710551aba6e05e2116e95521ff
David Thorpe <djt@mutablelogic.com>
2019-12-22 17:23:23 +0000

    added

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # googlecast
+
+[![CircleCI](https://circleci.com/gh/djthorpe/googlecast/tree/master.svg?style=svg)](https://circleci.com/gh/djthorpe/googlecast/tree/master)
+
 Manage Google Chromecast devices
--- cmd/googlecast-service/main.go
+++ cmd/googlecast-service/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"os"
+
+	// Frameworks
+	"github.com/djthorpe/gopi"
+)
+
+////////////////////////////////////////////////////////////////////////////////
+
+func Main(app *gopi.AppInstance, done chan<- struct{}) error {
+
+	app.Logger.Info("Waiting for CTRL+C")
+	app.WaitForSignal()
+	done <- gopi.DONE
+
+	// Success
+	return nil
+}
+
+////////////////////////////////////////////////////////////////////////////////
 
 func main() {
-	fmt.Println("Hello, world")
+	// Create the configuration
+	config := gopi.NewAppConfig("googlecast", "discovery")
+
+	// Run the command line tool
+	os.Exit(gopi.CommandLineTool2(config, Main))
 }
